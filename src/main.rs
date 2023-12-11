d272d7d02aaa0e490ee1a0f9bc362f71a36abc96
Matt <mnicholson110@gmail.com>
2023-12-11 12:59:32 -0600

    changes and such

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 use std::net::UdpSocket;
 use log::info;
 
-const SSID: &str = "notmyssid";
-const PASSWORD: &str = "notmypassword";
+const SSID: &str = "eventually I'm probably going to push my wifi info to a file and gitignore it";
+const PASSWORD: &str = "but not today";
 
 fn main() -> anyhow::Result<()> {
   esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
+  // Bind the log crate to the ESP Logging facilities
   EspLogger::initialize_default();
   let peripherals = Peripherals::take()?;
   let sys_loop = EspSystemEventLoop::take()?;
@@ -30,13 +30,21 @@ fn main() -> anyhow::Result<()> {
   info!("Wifi DHCP info: {:?}", ip_info);
 
   let socket = UdpSocket::bind("0.0.0.0:0")?;
-  let msg = r#"{"method":"setPilot","params":{"sceneId":12}}"#.to_string();
+  let msg_on = r#"{"method":"setPilot","params":{"sceneId":12}}"#.to_string();
+  let msg_party = r#"{"method":"setPilot","params":{"sceneId":4}}"#.to_string();
+  let light_addr = "192.168.4.145:38899";
 
-  socket.send_to(msg.as_bytes(), "192.168.4.145:38899")?;
   
   loop {
     // info!("Sleep!");
-    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // set light, wait 5 seconds, set light
+    socket.send_to(msg_on.as_bytes(), light_addr)?;
+    info!("Sent on!");
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    socket.send_to(msg_party.as_bytes(), light_addr)?; 
+    info!("Sent party!");
+    std::thread::sleep(std::time::Duration::from_secs(5));
   }
 
 
